024a90785350995500faa227c4d5a42986e9e322
Armando <piruk7@gmail.com>
2012-07-24 16:18:18 -0300

    Adicionado todos os .js ao precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,10 @@ Csilk::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( application.js )
+  #
+  Dir[Rails.root.join("app/assets/javascript/**/*.js")].each do |file|
+    config.assets.precompile += %w( file )
+  end
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
