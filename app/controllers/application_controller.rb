cf2aa418b6056f5919927cfe49ed22b16b8df0d8
Kako Botasso <kakodev@gmail.com>
2012-12-31 03:38:55 -0200

    Categoria de exibicao do menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :categorias
+  helper_method :categorias, :categoria_menu
 
   def categorias
   	@categorias = Categoria.all
   end
+
+  def categoria_menu
+  	f_id = Categoria.first.id
+  	l_id = Categoria.last.id
+  	random = rand(f_id...l_id)
+  	if Categoria.find_by_id(random).produtos.size > 0
+  		Categoria.find_by_id(random).slug
+  	else
+  		categoria_menu
+  	end
+  end
+
 end
