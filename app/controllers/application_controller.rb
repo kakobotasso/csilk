551a499fa68052ee593459697a34ac7b35b2ca8c
Kako Botasso <kakodev@gmail.com>
2012-12-31 11:59:28 -0200

    Correcao metodo categoria do menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,22 @@ class ApplicationController < ActionController::Base
   end
 
   def categoria_menu
-  	f_id = Categoria.first.id
-  	l_id = Categoria.last.id
-  	random = rand(f_id...l_id).to_i
-  	if Categoria.find_by_id(random).produtos.size > 0
-  		Categoria.find_by_id(random).slug
-  	else
-  		categoria_menu
-  	end
+    categoria = Categoria.all
+    categoria = categoria[ rand(categoria.size) ]
+
+    if categoria.produtos.size > 0
+      categoria.slug
+    else
+      categoria_menu
+    end
+  	#f_id = Categoria.first.id
+  	#l_id = Categoria.last.id
+  	#random = rand(f_id...l_id).to_i
+  	#if Categoria.find_by_id(random).produtos.size > 0
+  	#	Categoria.find_by_id(random).slug
+  	#else
+  	#	categoria_menu
+  	#end
   end
 
 end
