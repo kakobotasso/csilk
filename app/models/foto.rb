77ec251b59d0a5194a167e7602a5fb47cb222688
Kako Botasso <kakodev@gmail.com>
2013-01-30 23:38:31 -0200

    Ajustes no corte de imagens

--- app/models/foto.rb
+++ app/models/foto.rb
@@ -1,5 +1,5 @@
 class Foto < ActiveRecord::Base
   belongs_to :produto
 
-  has_attached_file :picture, :styles => { :menor => "52x52", :medio => "315x315", :homeprod => "183x138>", :original => "630x630" }
+  has_attached_file :picture, :styles => { :menor => "52x52#", :medio => "315x315#", :homeprod => "183x138#", :original => "630x630" }
 end
