af678992a4ebbeb460de7fc1209bd605041a881a
Kako Botasso <kakodev@gmail.com>
2013-02-14 21:55:19 -0200

    Alterando configuracao do CSS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,8 @@ Csilk::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :yui
+  #config.assets.js_compressor = :uglifier
+  #config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
